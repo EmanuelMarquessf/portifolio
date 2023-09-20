c1b23a22af185c5941c8daaafc22f9565bb9200e
Emanuel Fonseca <emanuel.marquessf@gmail.com>
2023-09-20 16:35:07 -0300

    deploy config

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,7 +3,7 @@
     <h2 class=" text-primaryColor font-poppins font-semibold text-[2rem]">Featured projects:</h2>
     <div class="flex flex-wrap gap-[1rem]">
       <div class="flex flex-col justify-between p-4 bg-[#31313f] rounded shadow grow gap-2">
-        <img class="rounded-sm w-full" src="../../public/Rectangle 21.png" alt="">
+        <img class="rounded-sm w-full" src="../../public/projectImg.png" alt="">
         <h3 class="text-primaryColor font-poppins text-[1.5rem] font-semibold">Em breve</h3>
         <p class="text-white text-[1.125rem] font-poppins font-medium w-[18rem]">Descrição do projeto</p>
         <div class="flex flex-row gap-4 mt-4">
